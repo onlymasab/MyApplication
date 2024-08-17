8d18d5c184b3e628f92f582da2f10bcd943b8e16
Masab Ejaz <maabejaz@icloud.com>
2024-08-17 08:44:45 +0500

    tesitng 2

--- app/src/main/kotlin/com/example/text/MainActivity.kt
+++ app/src/main/kotlin/com/example/text/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Github",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
