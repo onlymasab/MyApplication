14a57cbfd4746627661c358410c3b64fadde6160
Masab Ejaz <maabejaz@icloud.com>
2024-08-17 08:42:56 +0500

    testing

--- app/src/main/kotlin/com/example/text/MainActivity.kt
+++ app/src/main/kotlin/com/example/text/MainActivity.kt
@@ -11,14 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.text.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -27,7 +26,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
 }
 
 @Composable
@@ -38,10 +36,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
